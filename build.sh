b346ac9fc000f182b4431f6a242aee006a9ce52e
Connor Proctor <connorproctor@users.noreply.github.com>
2022-08-23 15:35:47 -0700

    v1

---
+++ build.sh
@@ -0,0 +1,2 @@
+# make zipfile for upload to cloudfront pages
+zip build_output.zip index.html FY2023_overlay_bikeways.geojson FY2023_slurry_bikeways.geojson planned_paving.geojson sexy_streets_merged.geojson bikeways_merged.geojson
---
+++ create_paving_bikelanes_geojson.rb
@@ -0,0 +1,201 @@
+lanes = [["III",		"SS-006403",	"SS-006404",	"SS-006402",	"SS-006401",],
+["II",		"SS-016239",	"SS-016240",	"SS-016241",],
+["IV",		"SS-019965",],
+["IV",		"SS-019967",	"SS-019968",],
+["II",		"SS-022982",],
+["II",		"SS-010937",],
+["II",		"SS-010935",	"SS-010934",],
+["III",		"SS-029102",],
+["II",		"SS-014805",	"SS-014801",	"SS-014800",	"SS-014804",	"SS-014799",	"SS-014798",	"SS-014803",	"SS-014797",	"SS-014796",	"SS-014795",	"SS-014802"],
+["II",		"SS-016998",],
+["II",		"SS-028143",	"SS-028150",	"SS-028142",	"SS-028141",],
+["II",		"SS-011244",],
+["II",		"SS-023145",	"SS-023144",	"SS-023143",	"SS-023142",	"SS-023141",	"SS-023140",	"SS-023139",],
+["II",		"SS-026332",	"SS-026331",],
+["III",		"SS-000179",	"SS-000178",],
+["III",		"SS-000592",	"SS-000591",	"SS-000590",	"SS-000589",],
+["II",		"SS-000871",],
+["IV",		"SS-004929",	"SS-004930",	"SS-004931",],
+["IV",		"SS-004927",],
+["III",		"SS-006755",	"SS-006756",	"SS-006758",	"SS-006761",	"SS-006760",],
+["III",		"SS-008751",	"SS-008752",],
+["IV",		"SS-000654",],
+["III",		"SS-020604",	"SS-020579",	"SS-020580",],
+["II",		"SS-010949",	"SS-003770",	"SS-003769",],
+["II",		"SS-004856",	"SS-004857",	"SS-004858",],
+["II",		"SS-009255",	"SS-009254",	"SS-009259",],
+["II",		"SS-010820",	"SS-010810",	"SS-010809",],
+["II",		"SS-010956",	"SS-010949",],
+["III",		"SS-020533",	"SS-020532",	"SS-020531",],
+["II",		"SS-021920",	"SS-021919",	"SS-021918",	"SS-021917",],
+["II",		"SS-023051",	"SS-023050",	"SS-023047",],
+["III",		"SS-029567",	"SS-029566",	"SS-029565",],
+["III",		"SS-022581",	"SS-022580",	"SS-022579",],
+["III",		"SS-028069",],
+["II",		"SS-031664",	"SS-032302",	"SS-032305",	"SS-032307",	"SS-032309",	"SS-030467",],
+["II",		"SS-022281",	"SS-022278",	"SS-022277",],
+["III",		"SS-030125",	"SS-030127",	"SS-030126",	"SS-030128",	"SS-030129",	"SS-030130",	"SS-030131",],
+["II",		"SS-006539",	"SS-006538",	"SS-006537",],
+["II",		"SS-006545",	"SS-006549",	"SS-006555",	"SS-006554",	"SS-006553",	"SS-006552",	"SS-006551",],
+["II",		"SS-030563",],
+["II",		"SS-020784",	"SS-020783",	"SS-020782",],
+["II",		"SS-025641",],
+["II",		"SS-027018",	"SS-027017",],
+["III",		"SS-030180",	"SS-030181",	"SS-030182",	"SS-006240",	"SS-006241",	"SS-006242",	"SS-006243",	"SS-006245",],
+["IV",		"SS-007296",	"SS-007297",	"SS-007312",],
+["II",		"SS-018595",	"SS-018604",	"SS-018605",	"SS-018596",	"SS-018597",],
+["II",		"SS-024207",	"SS-024208",	"SS-024204",	"SS-024205",],
+["III",		"SS-004652",	"SS-004653",	"SS-004654",	"SS-004655",	"SS-004656",],
+["II",		"SS-005740",],
+["II",		"SS-005754",],
+["II",		"SS-030672",],
+["II",		"SS-023247",	"SS-023248",],
+["II",		"SS-023245",],
+["III",		"SS-026095",	"SS-026099",],
+["II",		"SS-017473",	"SS-017472",],
+["II",		"SS-013672",	"SS-013677",	"SS-013676",	"SS-013670",	"SS-013669",],
+["III",		"SS-020606",	"SS-020583",	"SS-020584",	"SS-020586",	"SS-020607",	"SS-020588",	"SS-020608",	"SS-020589",],]
+
+overlay_lanes = [["III",	"SS-003338",	"SS-003339",	"SS-003340",	"SS-003337",	"SS-003341",	"SS-003342",],
+["IV",	"SS-028518",	"SS-028519",	"SS-028531",	"SS-028521",	"SS-028532",	"SS-028533",	"SS-028523",	"SS-028524",],
+["III",	"SS-004708",],
+["IV",	"SS-021183",	"SS-021182",	"SS-031326",],
+["II",	"SS-022355",	"SS-022354",],
+["III",	"SS-029255",],
+["III",	"SS-013601",	"SS-013602",	"SS-013581",	"SS-013603",	"SS-013604",	"SS-013582",	"SS-013585",	"SS-013584",	"SS-013583",],
+["III",	"SS-017100",],
+["II",	"SS-021164",	"SS-021149",],
+["II",	"SS-021163",	"SS-021162",],
+["II",	"SS-023056",],
+["II",	"SS-029640",	"SS-029639",	"SS-029641",	"SS-029638",	"SS-029637",],
+["II",	"SS-025090",	"SS-025091",	"SS-025092",],
+["IV",	"SS-003415",	"SS-003399",	"SS-003398",	"SS-003397",	"SS-003400",],
+["II",	"SS-005907",	"SS-005906",	"SS-005910",	"SS-031962",],
+["IV",	"SS-007284",	"SS-007285",	"SS-007286",	"SS-007287",],
+["II",	"SS-007870",	"SS-007869",	"SS-007868",	"SS-007867",	"SS-007866",	"SS-007865",	"SS-007864",	"SS-007863",	"SS-007862",	"SS-007861",	"SS-007860",	"SS-007859",	"SS-007858",],
+["II",	"SS-007582",],
+["II",	"SS-023781",],
+["II",	"SS-009263",	"SS-009256",	"SS-009258",	"SS-009261",	"SS-009265",	"SS-009264",	"SS-009257",],
+["II",	"SS-029268",	"SS-030015",	"SS-030014",],
+["III",	"SS-009785",	"SS-009784",],
+["III",	"SS-012929",	"SS-012930",],
+["II",	"SS-020757",],
+["II",	"SS-030746",	"SS-031670",	"SS-020763",	"SS-020764",	"SS-020765",],
+["II",	"SS-021748",],
+["III",	"SS-023964",	"SS-023965",],
+["III",	"SS-025930",],
+["III",	"SS-012928",	"SS-029831",	"SS-029830",],
+["II",	"SS-030017",	"SS-030010",	"SS-030011",	"SS-030012",	"SS-030018",	"SS-030013",	"SS-030014",	"SS-030015",	"SS-029268",],
+["III",	"SS-028508",],
+["III",	"SS-000967",],
+["II",	"SS-001037",	"SS-001036",],
+["III",	"SS-020612",	"SS-020596",	"SS-020613",	"SS-020597",	"SS-020598",	"SS-020614",	"SS-020599",	"SS-020615",	"SS-020600",	"SS-020616",	"SS-020617",],
+["III",	"SS-023013",	"SS-001348",],
+["II",	"SS-010578",	"SS-029603",	"SS-029604",	"SS-029615",	"SS-029605",],
+["II",	"SS-027121",]]
+
+require 'json'
+
+def class_to_name(type)
+    if type == "II"
+        "Unprotected Bike Lane (Class II)"
+    elsif type == "III"
+        "Sharrow (Class III)"
+    elsif type == "IV"
+        "Protected Bike Lane (Class IV)"
+    end 
+end
+
+seg_to_type = {}
+lanes.each do |lane|
+    type, *ids = lane
+    ids.each { |id| seg_to_type[id] = type }
+end
+
+segments_file = File.read('/Users/connor/Downloads/sd_paving_segs_datasd.geojson')
+segments_json = JSON.parse(segments_file)
+segment_lookup = {}
+segments_json["features"].each do |f|
+    segment_lookup[f['properties']['sapid']] = f
+end
+
+# updated_features = json["features"].each do |feature|
+#     type = seg_to_type[feature["properties"]["sapid"]]
+#     feature["properties"]["bike_class"] = type if type
+#     feature["properties"]["bike_class_name"] = class_to_name(type) if type
+# end
+
+geojson = {"type": "FeatureCollection", "features": []}
+seg_to_type.each_pair do |seg, type|
+    segment_info = segment_lookup[seg]["properties"]
+    segment_geometry = segment_lookup[seg]["geometry"]
+
+    feature = {
+        "type": "Feature",
+        "properties": {
+            road: segment_info["rd20full"],
+            bike_class: type,
+            bike_class_full: class_to_name(type),
+            lanes: segment_info["lane_no"],
+            width: segment_info["pwidth"],
+            speed: segment_info["speed"],
+            funclass: segment_info["funclass"],
+            sapid: segment_info["sapid"],
+            roadsegid: segment_info["roadsegid"],
+        },
+        "geometry": segment_geometry
+    }
+
+    geojson[:features] << feature
+end
+
+File.write('/Users/connor/Downloads/FY2023_slurry_bikeways.geojson', JSON.dump(geojson))
+
+
+require 'csv'
+
+# download from https://data.sandiego.gov/datasets/streets-repair-projects/
+paving_csv = CSV.read('/Users/connor/Downloads/sd_paving_datasd_v1.csv', headers: true)
+planned_paving_csv = paving_csv.filter { |row| ["bid / award", "design"].include?(row["status"]) }
+
+slurry_geojson = {"type": "FeatureCollection", "features": []}
+overlay_geojson = {"type": "FeatureCollection", "features": []}
+planned_paving_geojson = {"type": "FeatureCollection", "features": []}
+planned_paving_csv.each do |row|
+    seg = row["seg_id"]
+    segment_info = segment_lookup[seg]["properties"]
+    segment_geometry = segment_lookup[seg]["geometry"]
+    feature = {
+        "type": "Feature",
+        "properties": {
+            road: row["address_street"],
+            status: row["status"],
+            type: row["type"],
+            lanes: segment_info["lane_no"],
+            width: segment_info["pwidth"],
+            speed: segment_info["speed"],
+            project_id: row["project_id"],
+            project_manager: row["project_manager"],
+            project_manager_phone: row["project_manager_phone"],
+            resident_engineer: row["resident_engineer"],
+            date_moratorium: row["date_moratorium"],
+            date_start: row["date_start"],
+            date_end: row["date_end"],
+            seg_id: row["seg_id"],
+        },
+        "geometry": segment_geometry
+    }
+
+    planned_paving_geojson[:features] << feature
+    if row['type'] == 'Slurry'
+        slurry_geojson[:features] << feature
+    elsif row['type'] == 'Overlay'
+        overlay_geojson[:features] << feature
+    else
+        raise "unknown paving type"
+    end
+end
+
+File.write('/Users/connor/Downloads/planned_slurry.geojson', JSON.dump(slurry_geojson))
+File.write('/Users/connor/Downloads/planned_overlay.geojson', JSON.dump(overlay_geojson))
+File.write('/Users/connor/Downloads/planned_paving.geojson', JSON.dump(planned_paving_geojson))
