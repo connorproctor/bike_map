80f07c40f3b8217fa2ffe8d2f243d9bd23595633
Connor Proctor <connorproctor@users.noreply.github.com>
2022-12-29 20:06:22 -0800

    Google analytics

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-# make zipfile for upload to cloudfront pages
+# make zipfile for upload to cloudflare pages
 zip build_output.zip index.html FY2023_overlay_bikeways.geojson FY2023_slurry_bikeways.geojson sexy_streets_merged.geojson social_preview.png
